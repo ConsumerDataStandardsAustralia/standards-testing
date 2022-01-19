55b2350cb8ee1c42d719807be761f9913e1ab70f
James Bligh <james@redcrew.com.au>
2022-01-19 20:48:48 +1100

    Add cli skeleton

---
+++ tools/testdocs-cli/src/commands/validate.ts
@@ -0,0 +1,24 @@
+import type { Arguments, CommandBuilder } from 'yargs';
+
+type Options = {
+  filename: string;
+  follow: boolean | undefined;
+};
+
+export const command: string = 'validate <filename>';
+export const desc: string = 'Validate the tests in <filename> against the current testdocs schema';
+
+export const builder: CommandBuilder<Options, Options> = (yargs) =>
+  yargs
+    .options({
+      follow: { type: 'boolean' },
+    })
+    .positional('filename', { type: 'string', demandOption: true });
+
+export const handler = (argv: Arguments<Options>): void => {
+  const { filename, follow } = argv;
+  process.stdout.write(`Validating file named "${filename}"`);
+  if (follow) process.stdout.write(` following $refs`);
+  process.stdout.write('\n');
+  process.exit(0);
+};
