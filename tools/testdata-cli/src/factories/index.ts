d2438073c3ce2769ebe33eeda187c6da3d84d697
James Bligh <james@redcrew.com.au>
2022-08-07 22:27:20 +1000

    Minor format change

--- tools/testdata-cli/src/factories/index.ts
+++ tools/testdata-cli/src/factories/index.ts
@@ -1,7 +1,8 @@
 import { Factory } from '../logic/factoryService'
 
-// General loading factories
-import * as loadStatic from './loadStatic';
 
 export const factories: any = {}
+
+// General loading factories
+import * as loadStatic from './loadStatic';
 factories[loadStatic.factoryId] = (options: any): Factory => { return new loadStatic.factory(options) }
