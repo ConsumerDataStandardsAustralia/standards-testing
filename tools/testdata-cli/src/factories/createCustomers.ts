7156b557cd1e92e5361e5173fb0a4b1ff5946149
Tomas Schier <tomas@xpsolutions.com.au>
2023-01-03 18:22:26 +1100

    Added faker lib

--- tools/testdata-cli/src/factories/createCustomers.ts
+++ tools/testdata-cli/src/factories/createCustomers.ts
@@ -3,7 +3,7 @@ import * as fs from 'fs';
 import { ConsumerDataRightTestDataJSONSchema, Customer, CustomerWrapper, Holder, HolderWrapper } from 'src/logic/schema/cdr-test-data-schema';
 import { resourceLimits } from 'worker_threads';
 import { Factory, FactoryOptions, Helper } from '../logic/factoryService'
-
+import { faker } from '@faker-js/faker';
 const factoryId: string = "create-customers";
 
 export class CreateCustomers extends Factory {
@@ -23,8 +23,41 @@ export class CreateCustomers extends Factory {
   
     public canCreateCustomer(): boolean { return true; };
     public generateCustomer(): CustomerWrapper | undefined {
+      let rr = faker.name.firstName;
+      faker.locale = 'en_AU';
       let cust: Customer = {
-          firstName : "Tomas"
+          firstName : faker.name.firstName(),
+          lastName: faker.name.lastName(),
+          middleNames: [faker.name.middleName()],
+          phoneNumbers: [{
+            isPreferred: true,
+            purpose: 'HOME',
+            countryCode: '+61',
+            areaCode: '2',
+            number: faker.phone.number('0#-####-####'),
+            extension: 'x34',
+            fullNumber: faker.phone.number('+61 0#-#### ####')
+          }],
+          emailAddresses: [{
+            isPreferred: true,
+            purpose: 'HOME',
+            address: faker.internet.email()
+          }
+          ],
+          physicalAddresses: [ {
+             addressUtype: 'paf',
+             simple: {
+              mailingName: 'Home address',
+              addressLine1: faker.address.street(),
+              postcode: faker.address.zipCode('####'),
+              city: faker.address.city(),
+              state: faker.address.state(),
+              country: 'AUS'
+             }
+          }         
+          ]
+          
+          
       }
        let cw : CustomerWrapper = {
          customerId: Helper.randomId(),
