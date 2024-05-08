db9580a16148bc6b7542d7e3a7077565cd8efa87
abeheron <erez.ben-aharon@consumerdatastandards.gov.au>
2024-05-08 17:57:27 +1000

    Removed folders tools/testdata-cli and json-docs as the former is in its own repo, and the latter is not necessary anymore due to usage of testdoc-editor.

--- tools/testdata-cli/src/logic/factoryService/index.ts
+++
@@ -1,6 +0,0 @@
-// Base classes
-export * from './factory';
-export * from './helper';
-export * from './getFactoryIds';
-export * from './createFactory';
-export * from './generateData';
--- tools/testdata-cli/src/random-generators/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './random-banking';
-export * from './random-energy';
-export * from './random-common';
